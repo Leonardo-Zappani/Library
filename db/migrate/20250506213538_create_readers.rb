51ee4f5c5764a9f5fc477bab0a1e3127d084c3ce
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2025-05-06 18:36:30 -0300

    scaffolding readers

---
+++ db/migrate/20250506213538_create_readers.rb
@@ -0,0 +1,11 @@
+class CreateReaders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :readers do |t|
+      t.string :name
+      t.string :email
+      t.string :code
+
+      t.timestamps
+    end
+  end
+end
