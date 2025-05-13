c4e57979e786b2e9e15b90f85d17d9c634091118
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2025-05-13 20:49:38 -0300

    Adicionando intencionalmente code smells

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    # Adicionando um logger para verificar os usuários, de maneira errada
+    # pois independente da condição, o log será o mesmo, ou seja, é uma computação desnecessaria
+    if @users.any?
+      @users.each do |user|
+        if user.active?
+          logger.info user.email
+        else
+          logger.info user.email
+        end
+      end
+    end
   end
 
   def show; end
@@ -17,6 +29,8 @@ class UsersController < ApplicationController
   def edit; end
 
   def create
+    # Adicionando variavel nao utilizada.
+    user = User.new(user_params)
     @user = User.new(user_params)
 
     if @user.save
