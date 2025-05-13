68f46eac63536b9d512f18e0f9dd3fc2cec97b90
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2025-05-13 20:41:08 -0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,31 +4,53 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[show edit update destroy]
 
+  # Smell 1: Comentários multilinha vazios
+
   def index
+    # Smell 2: Variável local não utilizada
+    unused_count = User.count
+
     @users = User.all
-  end
 
-  def show; end
+    # Smell 3: Literais de string duplicados e código duplicado
+    notice_message = 'Operação realizada com sucesso.'
+    flash[:notice] = notice_message
+    flash[:alert]  = notice_message
 
-  def new
-    @user = User.new
-  end
-
-  def edit; end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to user_path(@user), notice: 'Usuário criado com sucesso.'
-    else
-      render :new, status: :unprocessable_entity
+    # Smell 4: Alta complexidade cognitiva (aninhamentos)
+    if @users.any?
+      @users.each do |user|
+        if user.active?
+          logger.info user.email
+        else
+          logger.info user.email
+        end
+      end
     end
   end
 
+  def create
+    @user = User.new(user_params)
+    # Smell 4 parte 2: Estrutura invertida de booleano
+    if !@user.save
+      render :new, status: :unprocessable_entity
+    else
+      redirect_to @user, notice: 'Usuário criado com sucesso.'
+    end
+  end
+
+  # Smell 5: Método com muitos parâmetros
+  def custom_action(a, b, c, d, e, f)
+    # lógica fictícia
+  end
+
+  def show; end
+  def new; @user = User.new; end
+  def edit; end
+
   def update
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: 'Usuário atualizado com sucesso.'
+      redirect_to @user, notice: 'Usuário atualizado com sucesso.'
     else
       render :edit, status: :unprocessable_entity
     end
